776b315250be5e9321b625a0b2e7fa9c81261bb8
Vertigeux <jiffyeljefe@gmail.com>
2024-08-25 16:09:32 -0400

    Updating meta data

--- frontend/src/common/footer.js
+++ frontend/src/common/footer.js
@@ -2,11 +2,14 @@ import { NavLink } from 'react-router-dom'
 import './css/footer.css'
 
 export default function Footer() {
-    return <footer className='footer'>
-        <p>A bot by Jiffboy</p>
-        <nav className="footer-navbar">
-            <NavLink to="/tos" className='footer-link' isActive={true}>Terms of Service</NavLink>
-            <NavLink to="/privacy" className='footer-link'>Privacy Policy</NavLink>
-        </nav>
-    </footer>
+    return <div className='footer-container'>
+    <hr className='footer-line'/>
+        <footer className='footer'>
+            <p>A bot by Jiffboy</p>
+            <nav className="footer-navbar">
+                <NavLink to="/tos" className='footer-link' isActive={true}>Terms of Service</NavLink>
+                <NavLink to="/privacy" className='footer-link'>Privacy Policy</NavLink>
+            </nav>
+        </footer>
+    </div>
 }
--- frontend/src/home/homePage.js
+++ frontend/src/home/homePage.js
@@ -1,7 +1,8 @@
 import React from 'react'
+import '../common/css/textPageContainer.css'
 
 export default function HomePage() {
-    return <div>
+    return <div className="text-page-container">
         <h1>Welcome to Jif Bot, a shitpost bot turned personal assistant. Designed specifically to meet my own needs, Jif Bot has various functionality to aid in discussion, provide basic tools, and bolsters some additional goodies that nobody wants or needs! Feel free to add my son to your server and give it a try: </h1>
         <button>Invite me!</button>
         <h2>Some basic features:</h2>
